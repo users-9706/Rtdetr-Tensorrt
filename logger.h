a24adf688ce452aedf178c12b378ecf50013844d
Justin <96218595+users-9706@users.noreply.github.com>
2023-12-28 22:44:23 +0800

    logger.h

---
+++ logger.h
@@ -0,0 +1,22 @@
+
+#undef UNICODE
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include "logging.h"
+
+class SampleErrorRecorder;
+extern SampleErrorRecorder gRecorder;
+namespace sample
+{
+extern Logger gLogger;
+extern LogStreamConsumer gLogVerbose;
+extern LogStreamConsumer gLogInfo;
+extern LogStreamConsumer gLogWarning;
+extern LogStreamConsumer gLogError;
+extern LogStreamConsumer gLogFatal;
+
+void setReportableSeverity(Logger::Severity severity);
+} // namespace sample
+
+#endif // LOGGER_H
