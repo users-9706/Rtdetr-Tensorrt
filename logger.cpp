ea6032b064632f52009e31ccf2c6b5ef4f4558b2
Justin <96218595+users-9706@users.noreply.github.com>
2023-12-28 22:45:34 +0800

    logger.cpp

--- logger.cpp
+++ logger.cpp
@@ -1,1 +1,26 @@
 
+
+#include "logger.h"
+#include "ErrorRecorder.h"
+#include "logging.h"
+using namespace nvinfer1;
+SampleErrorRecorder gRecorder;
+namespace sample
+{
+Logger gLogger{Logger::Severity::kINFO};
+LogStreamConsumer gLogVerbose{LOG_VERBOSE(gLogger)};
+LogStreamConsumer gLogInfo{LOG_INFO(gLogger)};
+LogStreamConsumer gLogWarning{LOG_WARN(gLogger)};
+LogStreamConsumer gLogError{LOG_ERROR(gLogger)};
+LogStreamConsumer gLogFatal{LOG_FATAL(gLogger)};
+
+void setReportableSeverity(Logger::Severity severity)
+{
+    gLogger.setReportableSeverity(severity);
+    gLogVerbose.setReportableSeverity(severity);
+    gLogInfo.setReportableSeverity(severity);
+    gLogWarning.setReportableSeverity(severity);
+    gLogError.setReportableSeverity(severity);
+    gLogFatal.setReportableSeverity(severity);
+}
+} // namespace sample
