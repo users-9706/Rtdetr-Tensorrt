c0e36125e581311e3191bd487165bb484f813ea3
Justin <96218595+users-9706@users.noreply.github.com>
2023-12-29 19:43:14 +0800

    Update logger.cpp

--- logger.cpp
+++ logger.cpp
@@ -1,5 +1,3 @@
-
-
 #include "logger.h"
 #include "ErrorRecorder.h"
 #include "logging.h"
@@ -7,20 +5,19 @@ using namespace nvinfer1;
 SampleErrorRecorder gRecorder;
 namespace sample
 {
-Logger gLogger{Logger::Severity::kINFO};
-LogStreamConsumer gLogVerbose{LOG_VERBOSE(gLogger)};
-LogStreamConsumer gLogInfo{LOG_INFO(gLogger)};
-LogStreamConsumer gLogWarning{LOG_WARN(gLogger)};
-LogStreamConsumer gLogError{LOG_ERROR(gLogger)};
-LogStreamConsumer gLogFatal{LOG_FATAL(gLogger)};
-
-void setReportableSeverity(Logger::Severity severity)
-{
-    gLogger.setReportableSeverity(severity);
-    gLogVerbose.setReportableSeverity(severity);
-    gLogInfo.setReportableSeverity(severity);
-    gLogWarning.setReportableSeverity(severity);
-    gLogError.setReportableSeverity(severity);
-    gLogFatal.setReportableSeverity(severity);
-}
-} // namespace sample
+    Logger gLogger{Logger::Severity::kINFO};
+    LogStreamConsumer gLogVerbose{LOG_VERBOSE(gLogger)};
+    LogStreamConsumer gLogInfo{LOG_INFO(gLogger)};
+    LogStreamConsumer gLogWarning{LOG_WARN(gLogger)};
+    LogStreamConsumer gLogError{LOG_ERROR(gLogger)};
+    LogStreamConsumer gLogFatal{LOG_FATAL(gLogger)};
+    void setReportableSeverity(Logger::Severity severity)
+    {
+        gLogger.setReportableSeverity(severity);
+        gLogVerbose.setReportableSeverity(severity);
+        gLogInfo.setReportableSeverity(severity);
+        gLogWarning.setReportableSeverity(severity);
+        gLogError.setReportableSeverity(severity);
+        gLogFatal.setReportableSeverity(severity);
+    }
+} 
