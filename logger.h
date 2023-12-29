82db4786cf0d47a20948b450a39f2b4a9c74a31e
Justin <96218595+users-9706@users.noreply.github.com>
2023-12-29 19:42:36 +0800

    Update logger.h

--- logger.h
+++ logger.h
@@ -1,22 +1,18 @@
-
 #undef UNICODE
 #ifndef LOGGER_H
 #define LOGGER_H
-
 #include "logging.h"
-
 class SampleErrorRecorder;
 extern SampleErrorRecorder gRecorder;
 namespace sample
 {
-extern Logger gLogger;
-extern LogStreamConsumer gLogVerbose;
-extern LogStreamConsumer gLogInfo;
-extern LogStreamConsumer gLogWarning;
-extern LogStreamConsumer gLogError;
-extern LogStreamConsumer gLogFatal;
+	extern Logger gLogger;
+	extern LogStreamConsumer gLogVerbose;
+    extern LogStreamConsumer gLogInfo;
+    extern LogStreamConsumer gLogWarning;
+    extern LogStreamConsumer gLogError;
+    extern LogStreamConsumer gLogFatal;
+    void setReportableSeverity(Logger::Severity severity);
+} 
+#endif 
 
-void setReportableSeverity(Logger::Severity severity);
-} // namespace sample
-
-#endif // LOGGER_H
